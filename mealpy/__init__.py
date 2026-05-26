f1d1273e2b21fadf62c18ab1cfe4af31492f5cb0
SealtielFreak <sealtiel@email.com>
2026-05-26 13:15:57 -0600

    Change version

--- mealpy/__init__.py
+++ mealpy/__init__.py
@@ -4,7 +4,7 @@
 #       Github: https://github.com/thieu1995        %
 # --------------------------------------------------%
 
-__version__ = "2026.1"
+__version__ = "2026.2"
 
 import inspect
 import sys
