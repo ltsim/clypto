70bec113d492bb0eaee9e3bff85f64e8ddc79ddb
SealtielFreak <sealtiel@email.com>
2026-05-25 18:15:08 -0600

    Change version

--- mealprint/__init__.py
+++ mealprint/__init__.py
@@ -4,7 +4,7 @@
 #       Github: https://github.com/thieu1995        %
 # --------------------------------------------------%
 
-__version__ = "0.0.0.1"
+__version__ = "2026"
 
 import inspect
 import sys
