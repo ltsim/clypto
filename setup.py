64674cef55fc5f7d91092101695f62ea5ce1eb80
SealtielFreak <sealtiel@email.com>
2026-05-28 18:32:56 -0600

    Version with Cython

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+from Cython.Build import cythonize
+
+def get_ext_modules():
+    return cythonize(
+        ["mealpy/**/*.py"],
+        exclude=["mealpy/**/__init__.py", "mealpy/collection/**/*.py"],
+        compiler_directives={"language_level": "3", "always_allow_keywords": True}
+    )
+
+setup(
+    ext_modules=get_ext_modules(),
+)
