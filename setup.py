18f82ff20ac44a09168c58a61a88c0eb7a9673e2
SealtielFreak <sealtiel@email.com>
2026-05-29 11:21:56 -0600

    Fix setup

--- setup.py
+++ setup.py
@@ -5,31 +5,8 @@ from Cython.Build import cythonize
 
 
 def get_ext_modules():
-    if os.name == 'nt':
-        extra_compile_args = ["/O2", "/Oi", "/Ot", "/GL", "/fp:fast", "/Qpar"]
-        extra_link_args = ["/LTCG"]
-    else:
-        extra_compile_args = [
-            "-O3",
-            "-ffast-math",
-            "-march=native",
-            "-flto",
-            "-funroll-loops",
-            "-fno-semantic-interposition"
-        ]
-        extra_link_args = ["-flto"]
-
-    extensions = [
-        Extension(
-            name=None,
-            sources=["mealpy/**/*.py"],
-            extra_compile_args=extra_compile_args,
-            extra_link_args=extra_link_args,
-        )
-    ]
-
     return cythonize(
-        extensions,
+        "mealpy/**/*.py",
         exclude=[
             "mealpy/**/__init__.py",
         ],
