2c70196fe185b859835b1c4fbb94b3c4ae22967f
Sealtiel <sealtiel@email.com>
2026-05-28 23:38:51 -0600

    Improvement Cython setup

--- setup.py
+++ setup.py
@@ -1,19 +1,53 @@
-from setuptools import setup, find_packages
+import os
+
+from setuptools import setup, Extension
 from Cython.Build import cythonize
 
 
 def get_ext_modules():
+    if os.name == 'nt':
+        extra_compile_args = ["/O2", "/Oi", "/Ot", "/GL", "/fp:fast", "/Qpar"]
+        extra_link_args = ["/LTCG"]
+    else:
+        extra_compile_args = [
+            "-O3",
+            "-ffast-math",
+            "-march=native",
+            "-flto",
+            "-funroll-loops",
+            "-fno-semantic-interposition"
+        ]
+        extra_link_args = ["-flto"]
+
+    extensions = [
+        Extension(
+            name=None,
+            sources=["mealpy/**/*.py"],
+            extra_compile_args=extra_compile_args,
+            extra_link_args=extra_link_args,
+        )
+    ]
+
     return cythonize(
-        ["mealpy/**/*.py"],
+        extensions,
         exclude=[
             "mealpy/**/__init__.py",
-            "mealpy/collection/**/*.py"
         ],
         compiler_directives={
             "language_level": "3",
-            "always_allow_keywords": True
+            "always_allow_keywords": True,
+            "boundscheck": False,
+            "wraparound": False,
+            "nonecheck": False,
+            "initializedcheck": False,
+            "cdivision": True,
+            "binding": False,
+            "overflowcheck": False,
+            "profile": False,
+            "linetrace": False
         }
     )
+
 
 setup(
     ext_modules=get_ext_modules(),
