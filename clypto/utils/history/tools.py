0fbff131c954d684e66906f9219139b90e0c803c
SealtielFreak <sealtiel@email.com>
2026-06-03 17:47:50 -0600

    Implemented function for detect stags

---
+++ clypto/utils/history/tools.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+from clypto.agents import Agent
+
+
+def global_repeated_times(global_best: list[Agent], epsilon: float) -> int:
+    count = 0
+
+    for idx in range(0, len(global_best) - 1):
+        temp = np.abs(
+            global_best[idx].target.fitness - global_best[idx + 1].target.fitness
+        )
+
+        if temp <= epsilon:
+            count += 1
+        else:
+            count = 0
+
+    return count
